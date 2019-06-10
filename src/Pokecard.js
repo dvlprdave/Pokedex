0c8af8a39818a10b7d9b53b1e0badee40a128d37
David <devdavee@gmail.com>
2019-06-10 07:01:20 -0400

    Winning / Losing hand and styles

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './Pokecard.css'
 
-// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 const POKE_API = ' https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 
 let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
@@ -12,7 +11,9 @@ class PokeCard extends Component {
     return (
       <div className='Pokecard'>
         <h1 className='Pokecard-title'>{this.props.name}</h1>
-        <img src={imgSrc} alt={this.props.name} />
+        <div className='Pokecard-image'>
+          <img src={imgSrc} alt={this.props.name} />
+        </div>
         <p className='Pokecard-data'>Type: {this.props.type}</p>
         <p className='Pokecard-data'>EXP: {this.props.exp}</p>
       </div>
